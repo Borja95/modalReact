94cce3ba2bc930cef728ca14143ae055c45ae176
FRANCISC0 RODRIGUEZ <abj951122@gmail.com>
2020-04-05 21:20:25 -0500

    version 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Input, Label} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state={
+    abierto: false,
+  }
+
+  abrirModal=()=>{
+    this.setState({abierto: !this.state.abierto});
+  }
+
+  render(){
+
+    const modalStyles={
+      position: "absolute",
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)'
+    }
+    return(
+      <>
+      <div className="principal">
+        <div className="secundario">
+      <Button color="success" onClick={this.abrirModal}>Mostrar Modal</Button>
+
+      </div></div>
+
+      <Modal isOpen={this.state.abierto} style={modalStyles}>
+        <ModalHeader>
+          Iniciar Sesión
+        </ModalHeader>
+        <ModalBody>
+          <FormGroup>
+            <Label for="usuario">Usuario</Label>
+            <Input type="text" id="usuario"/> 
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Contraseña</Label>
+            <Input type="text" id="password"/> 
+          </FormGroup>
+        </ModalBody>
+
+        <ModalFooter>
+            <Button color="primary">Iniciar Sesión</Button>
+            <Button color="secondary" onClick={this.abrirModal}>Cerrar</Button>
+        </ModalFooter>
+      </Modal>
+      </>
+    )
+  }
 }
 
 export default App;
